ded21d2bb54b7f42ea5ecd1dd1ca40752b1a8f02
Richard Turner <richardturnerdeveloper@gmail.com>
2017-07-13 16:32:56 -0400

    update helper function, finish header and footer template

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const port = process.env.PORT || 3000;
+const hbs = require('hbs');
+
 var app = express();
 
 app.set('view-engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getDate', () => {
+  return new Date().getFullYear();
+})
 
 
 app.get("/", (req,res) => {
-  res.render('home.hbs');
+  res.render('home.hbs', {
+    welcomeMessage: 'welcomeMessage'
+  });
 });
 
 app.get("/about", (req,res) => {
   res.send('A simple git test');
 });
+
+app.get("/projects", (req,res) => {
+  res.send('projects page');
+}
 
 app.get("/bad", (req,res) => {
   res.send({errorMessage: 'Cannot find this page!'});
